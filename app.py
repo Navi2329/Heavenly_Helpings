e1a3708c3ff78e6aa9f2f40c2ca2d09ead40d90f
Mohnish Karthikeyan <mohnish1612@gmail.com>
2023-07-08 19:36:24 +0530

    Modifying the Initail Files

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/donation')
+def donation():
+    return render_template('donation.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+if __name__ == '__main__':
+    app.run()
