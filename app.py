6ae4c8ec0bcc966c586a86956d9b9f0150f1a583
Navaneetha Krishnan <knavaneetha365@gmail.com>
2023-07-08 20:54:35 +0530

    flask integration

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
 def index():
     return render_template('index.html')
 
@@ -13,10 +17,6 @@ def about():
 @app.route('/donation')
 def donation():
     return render_template('donation.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 @app.route('/menu')
 def menu():
